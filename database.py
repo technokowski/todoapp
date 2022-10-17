ce74ebfabab48738b9c3cb0877440f6023aaaa16
technokowski <technokowski@LCARS-M2AIR.local>
2022-10-16 17:15:43 -0700

    changed from postgres to sqlite and added todos.db

--- database.py
+++ database.py
@@ -3,16 +3,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # For SQLite
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 # For PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234@localhost/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234@localhost/TodoApplicationDatabase"
 engine = create_engine(
     # For SQLite
-    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 
     # For PostgreSQL
-    SQLALCHEMY_DATABASE_URL
+    # SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
