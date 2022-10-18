eb2281b97399e16c50a4e47491efc308753c8010
technokowski <technokowski@LCARS-M2AIR.local>
2022-10-18 12:10:12 -0700

    changed token expiry

--- routers/auth.py
+++ routers/auth.py
@@ -62,12 +62,9 @@ def authenticate_user(username: str, password: str, db):
 def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta] = None):
     encode = {"sub": username, "id": user_id}
     if expires_delta:
-        # expire = datetime.utcnow() + expires_delta
-        expire = False
-
+        expire = datetime.utcnow() + expires_delta
     else:
-        # expire = datetime.utcnow() + timedelta(minutes=15)
-        expire = False
+        expire = datetime.utcnow() + timedelta(minutes=15)
     encode.update({"exp": expire})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
