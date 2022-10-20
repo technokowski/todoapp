5cc0219ee510b70274535420e949bb4ba17ba954
technokowski <technokowski@LCARS-M2AIR.local>
2022-10-19 21:02:13 -0700

    added the stocks feature to stocks branch

---
+++ routers/extras.py
@@ -0,0 +1,52 @@
+import sys
+# from urllib.request import Request
+sys.path.append("..")
+
+from starlette.responses import RedirectResponse
+from starlette import status
+from fastapi import Depends, APIRouter, Request, Form
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from .auth import get_current_user
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from yahoo_fin.stock_info import get_data
+import yahoo_fin.stock_info as si
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+
+router = APIRouter(
+    prefix="/extras",
+    tags=["extras"],
+    responses={404: {"description": "Not found"}}
+)
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/getstocks")
+async def get_stocks(request: Request):
+    x = await do_magic()
+    print(x)
+    msg = x[0]
+    msg_c = x[1]
+    msg_h = x[2]
+    return templates.TemplateResponse("stocks.html", {"request": request, "msg": msg, "msg_c": msg_c, "msg_h": msg_h})
+
+async def do_magic():
+    quote_table = si.get_quote_table("^GSPC", dict_result=False)
+    fifty_two_wh = quote_table['value'].loc[quote_table.index[0]]
+    x = fifty_two_wh.split(" - ")
+    high52 = x[1]
+    hight_no_comma = high52.replace(',',"")
+    f_high = float(hight_no_comma)
+
+    current_value = round(quote_table['value'].loc[quote_table.index[5]],3)
+
+    dads_magic = round(current_value / f_high * 100 - 100, 3)
+    return dads_magic, current_value, f_high
--- routers/todos.py
+++ routers/todos.py
@@ -12,6 +12,11 @@ from .auth import get_current_user
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from yahoo_fin.stock_info import get_data
+import yahoo_fin.stock_info as si
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 # Basically, all routes must begin with this prefix. Like registering in Django. 
 # The main.py file will make an object of this and use it. 
@@ -119,8 +124,30 @@ async def complete_todo(request: Request, todo_id: int, db: Session = Depends(ge
 
 @router.get("/secret", response_class=HTMLResponse)
 async def secret_it(request: Request):
-    user = await get_current_user(request)
-    if user is None:
-        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+    # user = await get_current_user(request)
+    # if user is None:
+    #     return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
     msg = "magic"
-    return templates.TemplateResponse("secret.html", {"request": request, "msg": msg, "user": user})
+    return templates.TemplateResponse("secret.html", {"request": request, "msg": msg})
+
+@router.get("/stocks", response_class=HTMLResponse)
+async def stock_info(request: Request):
+    x = await do_magic()
+    print(x)
+    msg = x[0]
+    msg_c = x[1]
+    msg_h = x[2]
+    return templates.TemplateResponse("stocks.html", {"request": request, "msg": msg, "msg_c": msg_c, "msg_h": msg_h})
+
+async def do_magic():
+    quote_table = si.get_quote_table("^GSPC", dict_result=False)
+    fifty_two_wh = quote_table['value'].loc[quote_table.index[0]]
+    x = fifty_two_wh.split(" - ")
+    high52 = x[1]
+    hight_no_comma = high52.replace(',',"")
+    f_high = float(hight_no_comma)
+
+    current_value = round(quote_table['value'].loc[quote_table.index[5]],3)
+
+    dads_magic = round(current_value / f_high * 100 - 100, 3)
+    return dads_magic, current_value, f_high
